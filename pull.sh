c2f31c8669cd6e40e303a22ca64d80b86428dcca
Eric Hansen <ehansen@artisight.com>
2024-12-04 13:14:27 -0600

    Tweaks to models
    qwen2.5-coder:32b consumes a good amount of GPU memory. When I'm doing
    other GPU intensive tasks, this can cause me to run out of memory, which
    leads to various malfunctions. Use qwen2.5-coder:7b in these situations.

--- pull.sh
+++ pull.sh
@@ -1,1 +1,6 @@
-docker exec -it ollama-ollama-1 ollama pull llama3.2:3b nomic-embed-text qwen2.5-coder:32b starcoder2:15b
+docker exec -it ollama-ollama-1 ollama pull \
+    llama3.2:3b \
+    nomic-embed-text \
+    qwen2.5-coder:32b \
+    qwen2.5-coder:7b \
+    starcoder2:15b
