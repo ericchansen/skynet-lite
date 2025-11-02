876369650d458c94e8fdb44f96e62bc47b0720b0
Eric Hansen <ericchansen@gmail.com>
2025-11-02 10:22:33 -0600

    Pull one model at a time
    This script never worked. I don't think it was ever able to pull
    multiple models at once.

--- pull.sh
+++ pull.sh
@@ -1,6 +1,5 @@
-docker exec -it ollama-ollama-1 ollama pull \
-    llama3.2:3b \
-    nomic-embed-text \
-    qwen2.5-coder:32b \
-    qwen2.5-coder:7b \
-    starcoder2:15b
+docker exec -it skynet-lite-ollama-1 ollama pull deepseek-r1:14b
+# docker exec -it skynet-lite-ollama-1 ollama pull llama3.1:8b
+# docker exec -it skynet-lite-ollama-1 ollama pull nomic-embed-text:v1.5
+# docker exec -it skynet-lite-ollama-1 ollama pull qwen2.5-coder:7b
+# docker exec -it skynet-lite-ollama-1 ollama pull starcoder2:15b
